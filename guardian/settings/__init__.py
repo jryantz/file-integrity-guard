313efe1c67f023e05f0be00a70fde78c2adb13f8
Jonathan Yantz <jon@motorlabs.io>
2022-06-17 12:47:30 -0400

    Added settings module

---
+++ guardian/settings/__init__.py
@@ -0,0 +1,24 @@
+"""
+Settings for the File Integrity Guard package.
+"""
+
+# The secret key is used as a seed in the file hash.
+SECRET_KEY = ''
+
+# Use debug mode to enable in-depth logging.
+DEBUG = True
+
+# Runtime Config
+
+GUARDED_DIRS = [
+
+]
+
+# Reporting Config
+
+ADMINS = [
+
+]
+
+DEFAULT_FROM_EMAIL = ''
+
