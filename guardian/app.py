ff200f4319c89820c9ec52daeb30baa84fcb2bf0
Jonathan Yantz <jon@motorlabs.io>
2022-06-17 12:52:26 -0400

    Added the laucher file and the app launch class for the guardian module

---
+++ guardian/app.py
@@ -0,0 +1,10 @@
+import settings
+
+class AppConfig:
+    """
+    Class representing the File Integrity Guard application and its
+    default configuration.
+    """
+
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+import guardian
+
+if __name__ == '__main__':
+    guardian.app.AppConfig()
