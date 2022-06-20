4e16d517965c7f9b33ff1074b2b36679c1acd99e
Jonathan Yantz <jon@motorlabs.io>
2022-06-20 15:14:37 -0400

    Moved settings from a folder to a file

--- guardian/settings.py
+++ guardian/settings.py
@@ -2,6 +2,11 @@
 Settings for the File Integrity Guard package.
 """
 
+from pathlib import Path
+
+# Use this to build relative paths to this project.
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
 # The secret key is used as a seed in the file hash.
 SECRET_KEY = ''
 
