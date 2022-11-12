f8d2aa87726d51ab071b8fe94e5fc93193f3fc43
Jonathan Yantz <jon@motorlabs.io>
2022-11-12 13:31:59 -0500

    Moved from default tests.py to a nested structure

--- tests/test_package.py
+++ tests/test_package.py
@@ -4,7 +4,4 @@ from guardian import __version__
 
 class TestPackage(unittest.TestCase):
     def test_version(self):
-        self.assertEqual(__version__, '0.1.0')
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(__version__, "0.1.0-alpha.1")
