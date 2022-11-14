f4e1ad690dfed96a394b96d625e8c7fdf0b6cafb
Jonathan Yantz <jon@motorlabs.io>
2022-11-13 19:12:50 -0500

    Added the initial functionality of the app to the run command

--- guardian/core/management/commands/run.py
+++ guardian/core/management/commands/run.py
@@ -1,9 +1,19 @@
 
 
+from guardian.app import GuardianConfig
 from guardian.core.management.base import BaseCommand
+from guardian.scanner import Scanner
 
 class Command(BaseCommand):
     help = "Starts the file integrity guard."
 
     def handle(self, *args, **options):
-        print("handle(...)", args, options)
+        # Initialize the app.
+        GuardianConfig()
+
+        # Create a list of the files to be scanned.
+        scanner = Scanner()
+        files = scanner.get_files()
+
+        for file in files:
+            print(file)
