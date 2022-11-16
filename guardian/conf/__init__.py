dc87e8e33038fb80e7740e41d6e861ff50c24164
Jonathan Yantz <jon@motorlabs.io>
2022-11-16 16:57:10 -0500

    Added a settings module to handle user defined settings

---
+++ guardian/conf/__init__.py
@@ -0,0 +1,37 @@
+from guardian.settings import BASE_DIR, DEBUG, GUARDED_DIRS, GUARDED_FILES, GUARDIAN_CACHE_FILENAME
+
+class Settings:
+    """
+    Class representing the File Integrity Guard application and its
+    default configuration.
+    """
+
+    def __new__(self):
+        if not hasattr(self, 'instance'):
+            self.instance = super(Settings, self).__new__(self)
+        return self.instance
+
+    def __init__(self):
+        self.debug = True
+        if not hasattr(self, 'debug'):
+            self.debug = DEBUG
+
+        self.cache_filename = '.guardian'
+        if isinstance(GUARDIAN_CACHE_FILENAME, str) \
+           and len(GUARDIAN_CACHE_FILENAME) > 0 \
+           and GUARDIAN_CACHE_FILENAME[0:1] == '.':
+            self.cache_filename = GUARDIAN_CACHE_FILENAME
+
+        self.project_dir = None
+        if isinstance(BASE_DIR, str):
+            self.project_dir = BASE_DIR
+
+        self.guarded_dirs = []
+        if isinstance(GUARDED_DIRS, list):
+            self.guarded_dirs = GUARDED_DIRS
+
+        self.guarded_files = []
+        if isinstance(GUARDED_FILES, list):
+            self.guarded_files = GUARDED_FILES
+
+settings = Settings()
