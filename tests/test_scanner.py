c23139aeec261012c043a31dc0eb8027788efe73
Jonathan Yantz <jon@motorlabs.io>
2023-01-15 10:02:47 -0500

    Added check for error files

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -35,3 +35,19 @@ class TestScanner(unittest.TestCase):
         scanned_files: list[str] = [str(x) for x in scanner.get_files()]
         test_files: list[str] = [str(x) for x in files]
         self.assertEqual(scanned_files, test_files)
+
+    def test_with_error(self):
+        example_files = [
+            'example/test1.txt',
+            'example/test3.txt',
+        ]
+
+        paths: list[str] = [x for x in example_files]
+        scanner = Scanner(paths)
+
+        files: list[File] = [File('example/test3.txt')]
+        files = sorted(files, key=lambda x: str(x))
+
+        error_files: list[str] = [str(x) for x in scanner.get_errors()]
+        test_files: list[str] = [str(x) for x in files]
+        self.assertEqual(error_files, test_files)
